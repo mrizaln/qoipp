221910198e8edf353048148230a2bf3e1b1ba271
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2024-06-04 16:29:12 +0700

    Add fuzz test

---
+++ example/source/qoifuzz.cpp
@@ -0,0 +1,17 @@
+#include <qoipp.hpp>
+
+using qoipp::ByteSpan;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    auto bytes = reinterpret_cast<std::byte*>(const_cast<uint8_t*>(data));
+    auto span  = ByteSpan{ bytes, size };
+
+    try {
+        auto [decoded, error] = qoipp::decode(span);
+    } catch (const std::exception& e) {
+        /* ignore exception */
+    }
+
+    return 0;
+}
