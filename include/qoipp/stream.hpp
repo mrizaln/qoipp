2ee5fbd98d8465d86feddbb5fafecd7870901b4b
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-05-20 22:59:56 +0700

    Rework encoder and fix it not producing expected result

--- include/qoipp/stream.hpp
+++ include/qoipp/stream.hpp
@@ -5,10 +5,6 @@
 
 namespace qoipp
 {
-    Result<void> write_header(ByteSpan out_buf, Desc desc) noexcept;
-
-    Result<void> write_end_marker(ByteSpan out_buf) noexcept;
-
     struct StreamResult
     {
         std::size_t processed;
@@ -18,30 +14,73 @@ namespace qoipp
     class StreamEncoder
     {
     public:
-        StreamEncoder(Channels channels) noexcept;
+        StreamEncoder() noexcept;
+
+        /**
+         * @brief Prepare encoder and fill the out buffer with header data.
+         *
+         * @param out_buf The buffer to be written.
+         * @param desc The description of the image to be encoded later.
+         *
+         * This function will both prepare the internal state of the encoder and fill the `out_buf` with the
+         * header information for qoi. You should not call this function twice. To reuse this encoder and
+         * prepare for use for other image, you should call `finalize` first then you can `initialize` again.
+         *
+         * This function returns
+         * - `Error::Empty` if the length of the `out_buf` is zero,
+         * - `Error::TooShort` if the length of the buffer is less than header length,
+         * - `Error::TooBig` if the image is too big to process,
+         * - `Error::InvalidDesc` if any of the field of `Desc` contains invalid value, or
+         * - `Error::AlreadyInitialized` if the encoder is already initialized.
+         */
+        Result<void> initialize(ByteSpan out_buf, Desc desc) noexcept;
 
         /**
          * @brief Encode the pixel data into out.
          *
-         * @param out The output buffer.
+         * @param out_buf The output buffer.
          * @param in Input pixels.
          * @return StreamResult with `processed` field set to number of pixels processed and `written` as
          * number of bytes written into `out`.
+         *
+         * The length of `out_buf` must be no less than 5.
+         *
+         * This function returns
+         * - `Error::Empty` if the length of the `out_buf` or `in_buf` is zero,
+         * - `Error::TooShort` if the length of the buffer is less than 5, or
+         * - `Error::NotInitialized` if the encoder hasn't initialized yet.
          */
-        StreamResult encode(ByteSpan out, ByteCSpan in) noexcept;
+        Result<StreamResult> encode(ByteSpan out_buf, ByteCSpan in_buf) noexcept;
 
         /**
-         * @brief Reset the state of the encoder to initial state.
+         * @brief Finalize the encoding process for this particular stream.
+         *
+         * @param out_buf Output buffer
+         *
+         * This function will both reset the internal state of the encoder and fill the `out_buf` with end
+         * marker. Also, if there is still `OP_RUN` left it will be written to the buffer before the end
+         * marker. You can use `has_run_count` to check whether you need to reserve extra 1 byte for `OP_RUN`
+         * or not on top of end marker length.
+         *
+         * This function returns
+         * - `Error::Empty` if the length of the `out_buf` is zero, or
+         * - `Error::TooShort` if the length of the buffer is less than header length, or
+         * - `Error::NotInitialized` if the encoder is already initialized.
          */
-        void reset();
+        Result<void> finalize(ByteSpan out_buf) noexcept;
+
+        /**
+         * @brief Check whether an `OP_RUN` count stored is non-zero.
+         */
+        bool has_run_count() const noexcept { return m_run > 0; }
 
     private:
         using RunningArray = PixelArr<constants::running_array_size>;
 
-        Channels     m_channels;
-        Byte         m_run;
-        Pixel        m_prev;
-        RunningArray m_seen;
+        std::optional<Channels> m_channels;
+        Byte                    m_run;
+        Pixel                   m_prev;
+        RunningArray            m_seen;
     };
 
     class StreamDecoder
