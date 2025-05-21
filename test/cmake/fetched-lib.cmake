4f61ff6a7695146d0a8c438a11ab8e15f6607dd9
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-05-21 23:35:58 +0700

    Use update ut library

--- test/cmake/fetched-lib.cmake
+++ test/cmake/fetched-lib.cmake
@@ -3,16 +3,26 @@ include(FetchContent)
 set(FETCHCONTENT_QUIET OFF)
 
 FetchContent_Declare(
-  qoi
-  GIT_REPOSITORY https://github.com/phoboslab/qoi
-  GIT_TAG master)
+    qoi
+    GIT_REPOSITORY https://github.com/phoboslab/qoi
+    GIT_TAG master
+)
 FetchContent_MakeAvailable(qoi)
 
 add_library(qoi INTERFACE)
 target_include_directories(qoi SYSTEM INTERFACE ${qoi_SOURCE_DIR})
 
 FetchContent_Declare(
-  dtl-modern
-  GIT_REPOSITORY https://github.com/mrizaln/dtl-modern
-  GIT_TAG v1.0.0)
+    dtl-modern
+    GIT_REPOSITORY https://github.com/mrizaln/dtl-modern
+    GIT_TAG v1.0.0
+)
 FetchContent_MakeAvailable(dtl-modern)
+
+FetchContent_Declare(
+    ut
+    URL https://github.com/boost-ext/ut/archive/refs/tags/v2.3.1.tar.gz
+    URL_HASH MD5=e5be92edaeab228d1d4ccc87fed3faae
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+)
+FetchContent_MakeAvailable(ut)
