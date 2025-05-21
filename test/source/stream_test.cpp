4366c75870e2fef635e448a912250a3a4d6e4836
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-05-21 23:37:27 +0700

    Add test for stream decoder

--- test/source/stream_test.cpp
+++ test/source/stream_test.cpp
@@ -1,5 +1,6 @@
 #include "util.hpp"
 
+#include <qoipp/simple.hpp>
 #include <qoipp/stream.hpp>
 
 #include <thread>
@@ -70,8 +71,9 @@ int main()
     // ----
 
     const auto simple_cases = std::array{ test_case_3, test_case_4 };
+    const auto do_encode    = false;
 
-    for (auto i = 5u; i < 1024; ++i) {
+    for (auto i = 5u; do_encode and i < 1024; ++i) {
         "stream encoder should be able to encode image"_test = [&](auto&& input) {
             const auto& [desc, raw, qoi, _] = input;
 
@@ -93,9 +95,8 @@ int main()
                 auto res = encoder.encode(out, in);
                 expect(res.has_value()) << "encode should successful";
 
-                auto [read, write]  = res.value();
-                off                += read;
-                encoded.insert(encoded.end(), out.begin(), out.begin() + write);
+                off += res->processed;
+                encoded.insert(encoded.end(), out.begin(), out.begin() + res->written);
             }
 
             auto size       = encoded.size();
@@ -105,7 +106,55 @@ int main()
             encoder.finalize({ encoded.data() + size, additional });
 
             expect(that % qoi.size() == encoded.size());
-            expect(rr::equal(qoi, encoded)) << util::compare_detail(qoi, encoded, 32);
+            expect(rr::equal(qoi, encoded)) << util::lazy_compare(qoi, encoded, 1, 32);
         } | simple_cases;
     }
+
+    "stream decoder should be able to decode image"_test = [&](auto&& input) {
+        const auto& [desc, raw, qoi, _] = input;
+
+        auto start = fmt::format(
+            "decode start chan={} | buf={} | qoi={}", static_cast<int>(desc.channels), 256, qoi.size()
+        );
+        ut::log << start;
+        fmt::println("{}", start);
+
+        auto decoder = qoipp::StreamDecoder{};
+        auto decoded = ByteVec{};
+
+        auto parsed_desc = decoder.initialize({ qoi.data(), qoipp::constants::header_size }).value();
+        expect(desc == parsed_desc);
+
+        auto off = qoipp::constants::header_size;
+        auto out = ByteArr<256>{};
+
+        auto end = qoi.size() - qoipp::constants::end_marker_size;
+
+        while (off < end) {
+            auto in  = ByteCSpan{ qoi.data() + off, std::min(out.size(), end - off) };
+            auto res = decoder.decode(out, in);
+            expect(res.has_value()) << "decode should successful";
+
+            fmt::println("read={} | write={} | off={} | size={}", res->processed, res->written, off, end);
+
+            using namespace std::chrono_literals;
+            std::this_thread::sleep_for(50ms);
+
+            off += res->processed;
+            decoded.insert(decoded.end(), out.begin(), out.begin() + res->written);
+        }
+
+        while (decoder.has_run_count()) {
+            auto count = decoder.drain(out).value();
+            decoded.insert(decoded.end(), out.begin(), out.begin() + count);
+        }
+
+        decoder.reset();
+
+        expect(that % raw.size() == decoded.size());
+        expect(rr::equal(raw, decoded))
+            << util::lazy_compare(raw, decoded, static_cast<int>(desc.channels), 8);
+    } | simple_cases;
+    // } | std::array{ test_case_4 };
+    // } | std::array{ test_case_3 };
 }
