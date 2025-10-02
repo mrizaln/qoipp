73a3435197d99de8f7ef88bf1ec21541b638e049
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-10-02 12:48:37 +0700

    Update test fetch script

--- test/fetch_test_images.sh
+++ test/fetch_test_images.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-wget -P resources/ https://qoiformat.org/qoi_test_images.zip
-cd resources/
-unzip qoi_test_images.zip
+# https://stackoverflow.com/a/246128
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+if ! [[ -f "${SCRIPT_DIR}/resources/qoi_test_images.zip" ]]; then
+    echo "$(basename $0): test files does not exists, fetching them..."
+
+    wget -P "${SCRIPT_DIR}/resources/" --no-verbose --force-progress https://qoiformat.org/qoi_test_images.zip
+    cd "${SCRIPT_DIR}/resources/"
+    unzip qoi_test_images.zip
+else
+    echo "$(basename $0): test files already exists"
+fi
